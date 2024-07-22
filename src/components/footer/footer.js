ea878fa05af0a3844247706cdd5073a472c969b7
Luis Henrique <duimbispo@gmail.com>
2024-07-22 16:02:01 -0300

    Criando estrutura de pastas do projeto

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+import '../../App.css';
+
+function Footer() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          teste footer
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Footer;
