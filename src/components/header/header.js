7f1b51c1c26436c6a574c4fa2f15070f208598a7
Luis Henrique <duimbispo@gmail.com>
2024-07-24 16:09:57 -0300

    Ajustes no header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,27 +8,40 @@ const logo = process.env.PUBLIC_URL + '/assets/img/logo.png';
 function Header() {
   return (
       <header className="header">
-        <div>
+        <div className='align-self-center'>
             <img src={logo} alt='logo' className='imglogo'></img>
         </div>
 
-      {/* BARRA DE PESQUISA */}
-      <div className="search-bar">
-        <FaSearch className="search-icon" />
-        <input 
-          type="text" 
-          placeholder="Procure por produto, nome, marca..." 
-          className="search-input"
-        />
-      </div>
+        {/* BARRA DE PESQUISA */}
+        <div className='d-flex flex-column px-2 justify-content-end' style={{backgroundColor: "blue"}}>
+          <div className="search-bar">
+            <FaSearch className="search-icon" />
+            <input 
+              type="text" 
+              placeholder="Procure por produto, nome, marca..." 
+              className="search-input"
+            />
+          </div>
 
-      {/* ICONES */}
-      <div className="icon-container">
-        <FaPercent className="icon" />
-        <FaShoppingCart className="icon" />
-        <FaUser className="icon" />
-      </div>
+          {/* NAVEGAÇÃO DO HEADER */}
+          <nav className="navigation">
+            <ul className="nav-list" style={{backgroundColor: "yellow"}}>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#meuspedidos">Meus pedidos</a></li>
+              <li><a href="#atendimento">Atendimento</a></li>
+              <li><a href="#sobrenos">Sobre nós</a></li>
+              <li><a href="#plantasnativas">Plantas nativas</a></li>
+            </ul>
+          </nav>
 
+        </div>
+
+        {/* ICONES */}
+        <div className="icon-container align-self-center">
+          <FaPercent className="icon" />
+          <FaShoppingCart className="icon" />
+          <FaUser className="icon" />
+        </div>
       </header>
   );
 }
