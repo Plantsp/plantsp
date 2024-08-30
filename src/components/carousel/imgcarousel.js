44097c844587392a1f50101573bbe38fa706c217
Luis Henrique <duimbispo@gmail.com>
2024-08-30 14:20:35 -0300

    Adicionando os textos ao carrossel

--- src/components/carousel/imgcarousel.js
+++ src/components/carousel/imgcarousel.js
@@ -2,13 +2,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Autoplay } from 'swiper/modules';
+import './carousel.css';
 
 function ImgCarousel() {
-
     return (
         <div>
             <Swiper 
-                pagination={{ clickable: true}}
+                pagination={{ clickable: true }}
                 modules={[Pagination, Autoplay]}
                 slidesPerView={1}
                 className="mySwiper"
@@ -16,15 +16,33 @@ function ImgCarousel() {
                     delay: 5000,
                 }}
             >
-
-                <SwiperSlide style={{height:"680px"}}> <img className='object-fit-cover' src="/assets/img/i1.jpg" alt="planta"/> </SwiperSlide>
-                <SwiperSlide style={{height:"680px"}}><img className='object-fit-cover' src="/assets/img/i4.jpg" alt="planta"/></SwiperSlide>
-                <SwiperSlide style={{height:"680px"}}><img className='object-fit-cover h-100' src="/assets/img/i2.jpg" alt="planta"/></SwiperSlide>
-                <SwiperSlide style={{height:"680px"}}><img className='object-fit-cover h-100' src="/assets/img/i3.jpg" alt="planta"/></SwiperSlide>
+                <SwiperSlide style={{ position: 'relative', height:"640px" }}>
+                    <img className='object-fit-cover' src="/assets/img/i1.jpg" alt="planta" style={{ filter: "brightness(0.50)" }}/>
+                    <div className="carousel-text">
+                        Produtos por até <br/><span>50%</span> de desconto!
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide style={{ position: 'relative', height:"640px" }}>
+                    <img className='object-fit-cover' src="/assets/img/i4.jpg" alt="planta" style={{ filter: "brightness(0.55)" }}/>
+                    <div className="carousel-text">
+                    Renove seu jardim <br/>com <span>preços especiais</span>!
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide style={{ position: 'relative', height:"640px" }}>
+                    <img className='object-fit-cover h-100' src="/assets/img/i2.jpg" alt="planta" style={{ filter: "brightness(0.62)" }}/>
+                    <div className="carousel-text">
+                    Transforme sua casa <br/><span>com descontos</span>!
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide style={{ position: 'relative', height:"640px" }}>
+                    <img className='object-fit-cover h-100' src="/assets/img/i3.jpg" alt="planta" style={{ filter: "brightness(0.70)" }}/>
+                    <div className="carousel-text">
+                        <span>Frete grátis</span> nas compras<br/> acima de R$100!
+                    </div>
+                </SwiperSlide>
             </Swiper>
-
         </div>
     );
 }
-  
+
 export default ImgCarousel;
