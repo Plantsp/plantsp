896b963f9c4ca697e5ceb55451f2a252ee6a7b6d
Luis Henrique <duimbispo@gmail.com>
2024-09-01 20:34:28 -0300

    Criando e estilizando o componente SpecialOffer

---
+++ src/components/SpecialOffer/specialoffer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './SpecialOffer.css';
+const plantalddireito = process.env.PUBLIC_URL + '/assets/img/plantalddireito.png';
+
+function SpecialOffer() {
+  return (
+    <div className="offer-container">
+      {/* Lado esquerdo com as informações */}
+      <div className="offer-text">
+        <h1>Oferta <br/><span className="especial">ESPECIAL</span></h1>
+        <p>Procuramos criar uma ligação profunda entre o <br></br>software e o usuário.</p>
+        <div className="price-container">
+          <span className="price-original">De R$ 120,00</span>
+          <span className="price-discount">Por R$ 90,00</span>
+        </div>
+        <button className="btn-ver-mais">Ver mais</button>
+      </div>
+
+      {/* Lado direito com a imagem e fundo verde */}
+      <div className="offer-image-container">
+      <img src={plantalddireito} alt='planta' className='offer-image'></img>
+      </div>
+    </div>
+  );
+}
+
+export default SpecialOffer;
