848f6e4e6024270d39a72dd66a268775837b9918
Luis Henrique <duimbispo@gmail.com>
2024-09-04 21:30:02 -0300

    Adicionando rotas ao projeto RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/global.css';
+import { RouterProvider } from "react-router-dom";
+import { router } from './routes/routes';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
