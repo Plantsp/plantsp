bb3d1f7ff122e13e62cd21dad637f894ecf55a7f
Luis Henrique <duimbispo@gmail.com>
2024-10-04 18:21:01 -0300

    adicionando imagens para cada produto

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -5,7 +5,9 @@ export const listaProdutos = [
     "preco": 15.99,
     "desconto": 0.1,
     "categoria": "Plantas",
-    "avaliacao": 4.5
+    "avaliacao": 4.5,
+    "imagem": "/assets/img/plantasuculenta.jpg"
+    
   },
   {
     "nome": "Kit de Vasos de Cerâmica",
@@ -13,7 +15,8 @@ export const listaProdutos = [
     "preco": 29.99,
     "desconto": 0.15,
     "categoria": "Vasos",
-    "avaliacao": 4.7
+    "avaliacao": 4.7,
+    "imagem": "/assets/img/tresvasos.jpg"
   },
   {
     "nome": "Adubo Orgânico",
@@ -21,7 +24,8 @@ export const listaProdutos = [
     "preco": 19.99,
     "desconto": 0.2,
     "categoria": "Adubos",
-    "avaliacao": 4.6
+    "avaliacao": 4.6,
+    "imagem": "/assets/img/aduboorg1.jpg"
   },
   {
     "nome": "Regador de Metal",
@@ -29,7 +33,9 @@ export const listaProdutos = [
     "preco": 24.99,
     "desconto": 0.1,
     "categoria": "Ferramentas",
-    "avaliacao": 4.8
+    "avaliacao": 4.8,
+    "imagem": "/assets/img/regadormetal.jpg"
+    
   },
   {
     "nome": "Luvas de Jardinagem",
@@ -37,7 +43,8 @@ export const listaProdutos = [
     "preco": 9.99,
     "desconto": 0.05,
     "categoria": "Ferramentas",
-    "avaliacao": 4.4
+    "avaliacao": 4.4,
+    "imagem": "/assets/img/luvas.jpg"
   },
   {
     "nome": "Terra Vegetal",
@@ -45,7 +52,9 @@ export const listaProdutos = [
     "preco": 12.99,
     "desconto": 0.1,
     "categoria": "Terra",
-    "avaliacao": 4.5
+    "avaliacao": 4.5,
+    "imagem": "/assets/img/terra_vegetal.webp"
+    
   },
   {
     "nome": "Kit de Ferramentas para Bonsai",
@@ -53,15 +62,19 @@ export const listaProdutos = [
     "preco": 34.99,
     "desconto": 0.2,
     "categoria": "Ferramentas",
-    "avaliacao": 4.9
+    "avaliacao": 4.9,
+    "imagem": "/assets/img/ferramentas.jpg"
+    
   },
   {
-    "nome": "Planta Cacto",
+    "nome": "Kit de Planta Cacto",
     "descricao": "Cacto de pequeno porte ideal para ambientes internos.",
     "preco": 14.99,
     "desconto": 0.1,
     "categoria": "Plantas",
-    "avaliacao": 4.6
+    "avaliacao": 4.6,
+    "imagem": "/assets/img/kitcacto.jpg"
+    
   },
   {
     "nome": "Fertilizante Líquido",
@@ -69,7 +82,8 @@ export const listaProdutos = [
     "preco": 19.99,
     "desconto": 0.15,
     "categoria": "Adubos",
-    "avaliacao": 4.7
+    "avaliacao": 4.7,
+    "imagem": "/assets/img/fertilizanteliq1.jpg"
   },
   {
     "nome": "Tesoura de Poda",
@@ -77,7 +91,8 @@ export const listaProdutos = [
     "preco": 17.99,
     "desconto": 0.1,
     "categoria": "Ferramentas",
-    "avaliacao": 4.8
+    "avaliacao": 4.8,
+    "imagem": "/assets/img/tesoura.jpg"
   },
   {
     "nome": "Planta de Lavanda",
@@ -85,7 +100,8 @@ export const listaProdutos = [
     "preco": 16.99,
     "desconto": 0.1,
     "categoria": "Plantas",
-    "avaliacao": 4.6
+    "avaliacao": 4.6,
+    "imagem": "/assets/img/lavanda.jpg"
   },
   {
     "nome": "Sistema de Irrigação Automática",
@@ -93,7 +109,8 @@ export const listaProdutos = [
     "preco": 99.99,
     "desconto": 0.2,
     "categoria": "Ferramentas",
-    "avaliacao": 4.9
+    "avaliacao": 4.9,
+    "imagem": "/assets/img/regador.jpg"
   },
   {
     "nome": "Estufa para Plantas",
@@ -101,7 +118,8 @@ export const listaProdutos = [
     "preco": 59.99,
     "desconto": 0.15,
     "categoria": "Ferramentas",
-    "avaliacao": 4.7
+    "avaliacao": 4.7,
+    "imagem": "/assets/img/estufa.jpg"
   },
   {
     "nome": "Sementes de Flores",
@@ -109,7 +127,8 @@ export const listaProdutos = [
     "preco": 7.99,
     "desconto": 0.05,
     "categoria": "Sementes",
-    "avaliacao": 4.3
+    "avaliacao": 4.3,
+    "imagem": "/assets/img/sementesflores.webp"
   },
   {
     "nome": "Kit de Mini-Horta",
@@ -117,7 +136,8 @@ export const listaProdutos = [
     "preco": 24.99,
     "desconto": 0.1,
     "categoria": "Ferramentas",
-    "avaliacao": 4.5
+    "avaliacao": 4.5,
+    "imagem":"https://www.nattivusbrindes.com/uploads/product/d8ab7fceab2b13b560c10f98ffe5a56b.jpg"
   },
   {
     "nome": "Planta de Alecrim",
@@ -125,7 +145,8 @@ export const listaProdutos = [
     "preco": 12.99,
     "desconto": 0.1,
     "categoria": "Plantas",
-    "avaliacao": 4.6
+    "avaliacao": 4.6,
+    "imagem":"https://img.freepik.com/premium-photo/rosemary-grows-pot_154293-6730.jpg?w=740"
   },
   {
     "nome": "Composto Orgânico",
@@ -133,7 +154,8 @@ export const listaProdutos = [
     "preco": 22.99,
     "desconto": 0.2,
     "categoria": "Adubos",
-    "avaliacao": 4.7
+    "avaliacao": 4.7,
+    "imagem":"https://static.wixstatic.com/media/093c2e_5d7b8f9f1f3146528d7d84de0c376fb9~mv2.jpg/v1/fill/w_480,h_644,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/093c2e_5d7b8f9f1f3146528d7d84de0c376fb9~mv2.jpg"
   },
   {
     "nome": "Planta de Orquídea",
@@ -141,7 +163,8 @@ export const listaProdutos = [
     "preco": 29.99,
     "desconto": 0.15,
     "categoria": "Plantas",
-    "avaliacao": 4.8
+    "avaliacao": 4.8,
+    "imagem":"https://img.freepik.com/premium-photo/beautiful-blooming-orchid-jar-old-wooden-jewelry-box-white-table-near-beige-wall-space-text_495423-46077.jpg?w=740"
   },
   {
     "nome": "Kit de Ferramentas de Jardim",
@@ -149,7 +172,8 @@ export const listaProdutos = [
     "preco": 39.99,
     "desconto": 0.2,
     "categoria": "Ferramentas",
-    "avaliacao": 4.9
+    "avaliacao": 4.9,
+    "imagem":"https://img.irroba.com.br/filters:fill(fff):quality(80)/ritecaae/catalog/api/ritecaae_citelerp/6553b89538933.jpg"
   },
   {
     "nome": "Planta de Manjericão",
@@ -157,7 +181,8 @@ export const listaProdutos = [
     "preco": 11.99,
     "desconto": 0.1,
     "categoria": "Plantas",
-    "avaliacao": 4.6
+    "avaliacao": 4.6,
+    "imagem":"https://images.tcdn.com.br/img/img_prod/799330/manjericao_gemini_1805_1_20200920122250.jpg"
   },
   // {
   //   "nome": "Rosa Vermelha",
@@ -170,10 +195,11 @@ export const listaProdutos = [
   {
     "nome": "Vaso de Cerâmica",
     "descricao": "Vaso de cerâmica feito à mão, perfeito para plantas pequenas.",
-    "preco": 20.50,
+    "preco": 20.99,
     "desconto": 0.15,
     "categoria": "Vasos",
-    "avaliacao": 4.5
+    "avaliacao": 4.5,
+    "imagem":"https://m.media-amazon.com/images/I/71iQbkQY90L._AC_UF894,1000_QL80_.jpg"
   },
   {
     "nome": "Fertilizante Orgânico",
@@ -181,7 +207,8 @@ export const listaProdutos = [
     "preco": 5.75,
     "desconto": 0.05,
     "categoria": "Adubos",
-    "avaliacao": 4.7
+    "avaliacao": 4.7,
+    "imagem":"https://bomcultivo.cdn.magazord.com.br/img/2021/05/produto/2859/fertilizante-substrato-organico-esterco-de-galinhas-plantio-organico-bom-cultivo.jpg?ims=fit-in/800x800/filters:fill(white)"
   },
   {
     "nome": "Kit de Ferramentas para Jardinagem",
@@ -189,30 +216,34 @@ export const listaProdutos = [
     "preco": 35.00,
     "desconto": 0.2,
     "categoria": "Ferramentas",
-    "avaliacao": 4.9
-  },
+    "avaliacao": 4.9,
+    "imagem":"https://img.freepik.com/premium-photo/gardening-tools-wooden-background-flat-lay_753190-1123.jpg?w=740"
+},
   {
     "nome": "Semente de Girassol",
     "descricao": "Sementes de girassol de alta qualidade para cultivar em casa.",
     "preco": 3.99,
     "desconto": 0,
     "categoria": "Sementes",
-    "avaliacao": 4.6
-  },
+    "avaliacao": 4.6,
+    "imagem":"https://img.freepik.com/free-photo/black-sunflower-seeds-bowl-white-background-high-angle-view_176474-5185.jpg?t=st=1728075652~exp=1728079252~hmac=aa213a6933e9bc08ef5b5917742a0df22bbcf550589e67ef8885a5a483530e98&w=740"
+},
   {
     "nome": "Fertilizante Granulado",
     "descricao": "Fertilizante granulado de liberação lenta para todas as plantas.",
     "preco": 21.99,
     "desconto": 0.1,
     "categoria": "Adubos",
-    "avaliacao": 4.6
-  },
+    "avaliacao": 4.6,
+    "imagem":"https://www.adufertil.com.br/wp-content/uploads/2022/05/adubo-npk-preco-adufertil.jpg"
+},
   {
     "nome": "Fertilizante para Orquídeas",
     "descricao": "Fertilizante especialmente formulado para orquídeas.",
     "preco": 18.99,
     "desconto": 0.1,
     "categoria": "Adubos",
-    "avaliacao": 4.8
-  }
+    "avaliacao": 4.8,
+    "imagem":"https://images.tcdn.com.br/img/img_prod/1270823/forth_orquideas_floracao_400g_fertilizante_peters_2003_1_f758c134e4ec383dd636b634d6d580d0.jpg"
+}
 ]
