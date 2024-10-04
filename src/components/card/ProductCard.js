6d4da74f8c37da091f3b79c27e079193e80c5dae
Luis Henrique <duimbispo@gmail.com>
2024-10-04 18:19:28 -0300

    ajustes nas imagens dos produtos e adição de truncate no título

--- src/components/card/ProductCard.js
+++ src/components/card/ProductCard.js
@@ -4,7 +4,7 @@ import { FaHeart } from 'react-icons/fa'; // Importando o icone de coração
 
 function ProductCard({produto}) {
   const [isFavorited, setIsFavorited] = useState(false); // Estado para controlar se o produto é favorito
-  const planta = process.env.PUBLIC_URL + '/assets/img/planta.png';
+  const img = process.env.PUBLIC_URL + produto.imagem;
 
     // Função para alternar o estado de favorito
     const toggleFavorite = () => {
@@ -13,15 +13,15 @@ function ProductCard({produto}) {
 
   return (
     <div className="card w-100" style={{ cursor: "pointer" }} onClick={() => console.log("produto clicado: ", produto)}>
-        <img src={planta} alt='planta' className="card-img-top object-fit-cover" height={240} />
+        <img src={img} alt='planta' className="card-img-top object-fit-cover" height={240} />
 
         {/* Botão do ícone de coração */}
       <button className="btn btn-link position-absolute" style={{ top: '10px', right: '10px', color: isFavorited ? 'red' : '#331800', zIndex: 10 }} onClick={toggleFavorite}>
         <FaHeart size={24} />
       </button>
 
-        <div className="card-body">
-            <h5 className="card-title">{produto.nome}</h5>
+        <div className="card-body p-2">
+            <h5 className="card-title text-truncate">{produto.nome}</h5>
             <p className="card-text">R${produto.preco.toString().replace(".", ",")}</p>
             <ReactStars
               count={5}
