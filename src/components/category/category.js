3fd4787af0b6bfaf8b8ff50005e74db1a7718f3c
Luis Henrique <duimbispo@gmail.com>
2024-10-10 22:09:20 -0300

    Ajustando os cards categoria

--- src/components/category/category.js
+++ src/components/category/category.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col } from 'react-bootstrap';
 import { FaLeaf, FaSeedling, FaHandHoldingWater, FaTools } from 'react-icons/fa';
 import { GiCactusPot } from "react-icons/gi";
 import { GiGroundSprout } from "react-icons/gi";
@@ -23,20 +24,22 @@ function SetIcon(title){
     }
 }
 
-
 function Category({title, filterCategory, activeCategory}) {
     return (
-        <div 
-            className={`card-category my-5 d-flex flex-column align-items-center justify-content-center ${activeCategory === title ? 'active' : ''}`}
-            style={{ width: "18rem", cursor:"pointer"}}
-            onClick={() => filterCategory(title)}
-        >
-            {SetIcon(title)}
-           
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+        <Col xs={6} sm={6} md={4} lg={2} xl={2}>
+            <div 
+                className={`w-100 card-category my-5 d-flex flex-column align-items-center justify-content-center ${activeCategory === title ? 'active' : ''}`}
+                style={{ cursor:"pointer"}}
+                onClick={() => filterCategory(title)}
+            >
+                {SetIcon(title)}
+            
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                </div>
             </div>
-        </div>
+        </Col>
+        
     );
 }
 
@@ -45,14 +48,16 @@ function CategorySearch({filterCategory, activeCategory}){
         <section>
             <h1 className='title-top'>PESQUISAR POR CATEGORIAS</h1>
             
-            <div className="categories-container px-4" style={{ display: 'flex', justifyContent: 'center', gap: '20px'}}>
-              <Category title="Plantas" filterCategory={filterCategory} activeCategory={activeCategory}/> 
-              <Category title="Vasos" filterCategory={filterCategory} activeCategory={activeCategory}/>
-              <Category title="Adubos" filterCategory={filterCategory} activeCategory={activeCategory}/>
-              <Category title="Sementes" filterCategory={filterCategory} activeCategory={activeCategory}/>
-              <Category title="Ferramentas" filterCategory={filterCategory} activeCategory={activeCategory}/>
-              <Category title="Terra" filterCategory={filterCategory} activeCategory={activeCategory}/>
-            </div>
+                <div className="row px-4">
+                    <Category title="Plantas" filterCategory={filterCategory} activeCategory={activeCategory}/> 
+                    <Category title="Vasos" filterCategory={filterCategory} activeCategory={activeCategory}/>
+                    <Category title="Adubos" filterCategory={filterCategory} activeCategory={activeCategory}/>
+                    <Category title="Sementes" filterCategory={filterCategory} activeCategory={activeCategory}/>
+                    <Category title="Ferramentas" filterCategory={filterCategory} activeCategory={activeCategory}/>
+                    <Category title="Terra" filterCategory={filterCategory} activeCategory={activeCategory}/>
+                </div>
+            
+                
         </section>
     )
 }
