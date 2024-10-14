34c77c01e286324688024d22a5ce24f73c74b8b3
Luis Henrique <duimbispo@gmail.com>
2024-10-14 14:32:16 -0300

    Adicionando rota para a página sobre nós

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import Home from "../pages/home/home";
 import Login from "../pages/login/login";
 import Cadastrar from "../pages/cadastro/cadastrar";
 import Perfil from "../pages/profile/profile";
+import Sobre from "../pages/sobrenos/sobrenos";
 
 import { Route, Routes as RoutesDom } from "react-router-dom";
 
@@ -12,6 +13,7 @@ const Routes = () => {
         <Route path="login" element={<Login />} />
         <Route path="cadastrar" element={<Cadastrar />} />
         <Route path="perfil" element={<Perfil/>} />
+        <Route path="sobrenos" element={<Sobre/>} />
         
       </RoutesDom>
   )
