76980b6d9785eec6cd692f5cb374ebb3c79d679e
Luis Henrique <duimbispo@gmail.com>
2024-11-10 14:52:27 -0300

    Ajustes na tela perguntas

--- src/pages/faq/faq.js
+++ src/pages/faq/faq.js
@@ -2,7 +2,6 @@ import Header from '../../components/header/headerdesktop';
 import Footer from '../../components/footer/footer';
 import "./faq.css";
 
-
 function Faq() {
 
     return (
@@ -14,8 +13,8 @@ function Faq() {
                         <h2 id="cliente_pergunta">Faça sua pergunta e tire sua dúvida</h2>
                         <input type="text" placeholder="Digite aqui..." id="arera_pergunta"></input>
                         <div id="btns">
-                            <button class="perbtn" id="deletar" type='reset'>Deletar</button>
-                            <button class="perbtn" id="enviar" type='submit'>Enviar</button>
+                            <button className="perbtn" id="deletar" type='reset'>Deletar</button>
+                            <button className="perbtn" id="enviar" type='submit'>Enviar</button>
                         </div>
                     </div>
 
@@ -23,33 +22,33 @@ function Faq() {
 
                     <div id="pergunta_rep">
 
-                        <div class="caixa_pr">
-                            <h2 class="perguntaT">Pergunta:</h2>
-                            <p class="caixa_da_pergunta">Vocês vendem pão também?</p>
+                        <div className="caixa_pr">
+                            <h2 className="perguntaT">Pergunta:</h2>
+                            <p className="caixa_da_pergunta">Vocês vendem pão também?</p>
 
-                            <div class="sep">
-                                <h2 class="RespostT">Resposta:</h2>
-                                <p class="caixa_de_resposta">Claro que não krl kskskskks</p>
+                            <div className="sep">
+                                <h2 className="RespostT">Resposta:</h2>
+                                <p className="caixa_de_resposta">Claro que não krl kskskskks</p>
                             </div>
                         </div>
 
-                        <div class="caixa_pr">
-                            <h2 class="perguntaT">Pergunta:</h2>
-                            <p class="caixa_da_pergunta">Vocês vendem pão também?</p>
+                        <div className="caixa_pr">
+                            <h2 className="perguntaT">Pergunta:</h2>
+                            <p className="caixa_da_pergunta">Vocês vendem pão também?</p>
 
-                            <div class="sep">
-                                <h2 class="RespostT">Resposta:</h2>
-                                <p class="caixa_de_resposta">Claro que não krl kskskskks</p>
+                            <div className="sep">
+                                <h2 className="RespostT">Resposta:</h2>
+                                <p className="caixa_de_resposta">Claro que não krl kskskskks</p>
                             </div>
                         </div>
 
-                        <div class="caixa_pr">
-                            <h2 class="perguntaT">Pergunta:</h2>
-                            <p class="caixa_da_pergunta">Vocês vendem pão também?</p>
+                        <div className="caixa_pr">
+                            <h2 className="perguntaT">Pergunta:</h2>
+                            <p className="caixa_da_pergunta">Vocês vendem pão também?</p>
 
-                            <div class="sep">
-                                <h2 class="RespostT">Resposta:</h2>
-                                <p class="caixa_de_resposta">Claro que não krl kskskskks</p>
+                            <div className="sep">
+                                <h2 className="RespostT">Resposta:</h2>
+                                <p className="caixa_de_resposta">Claro que não krl kskskskks</p>
                             </div>
                         </div>
                     </div>  
