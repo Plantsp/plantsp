6a460593cb353622d9febc2451b044de6317f118
Luis Henrique <duimbispo@gmail.com>
2024-11-12 14:41:25 -0300

    Ajustes na tela produto

--- src/pages/produto/produto.js
+++ src/pages/produto/produto.js
@@ -1,66 +1,85 @@
-import React, {useEffect, useState} from "react";
-import "./produto.css"; 
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import Header from '../../components/header/headerdesktop';
 import Footer from '../../components/footer/footer';
+import { FaUndo, FaShieldAlt, FaTruck } from "react-icons/fa"; // Ícones do react-icons
 import { useParams } from "react-router-dom";
 import { listaProdutos } from "../../data/produtos";
+import "./produto.css";
 
-  function Produto (){
-    const {id} = useParams();
+function Produto() {
+  const { id } = useParams();
+  const [produtoInfo, setProdutoInfo] = useState({});
 
-   const [produtoInfo, setProdutoInfo] = useState({});
-   
-    useEffect(() => {
-        findProd(id);
-    },[id]);
+  useEffect(() => {
+    findProd(id);
+  }, [id]);
 
-    function findProd(value) {
-       let prod = listaProdutos.find((produto) => produto.id === value);
-       setProdutoInfo(prod);
-    }
-
-    return (
-        <div>
-        <Header></Header>
-    
-            <section className="prod_escolhido">
-               <div className="imgs_prod">
-                <img src={produtoInfo.imagem} alt="." className="img_inicial" />
-               </div>
-    
-               <div className="info_prod">
-                    <h2>{produtoInfo.nome}</h2>
-                    <p className="descricao">{produtoInfo.descricao}</p>
-                    <p className="preco">R$ {Number(produtoInfo.preco).toFixed(2).replace(".", ",")}</p>
-                    <p className="desc">R$ {(produtoInfo.preco * (1 - produtoInfo.desconto)).toFixed(2).replace(".", ",")}</p>
-                    {/* <p className="desc">Desconto: {produtoInfo.desconto * 100}%</p> */}
-                    {/* <p className="estoque">Estoque: 23 disponiveis</p> */}
-    
-                    <h3>Quantidade:</h3>
-                    <div className="div_btn_quant">
-                        <button className="aumentar">+</button>
-                        <p className="quant">12</p>
-                        <button className="diminuir">-</button>
-                    </div>
-    
-                    <button className="btn_compra">COMPRAR</button>
-                    {/* <button className="btn_add">ADD</button> */}
-               </div>
-            </section>
-    
-            <section className="info_entrega_prod">
-                <div className="infos"></div>
-                <div className="infos"></div>
-                <div className="infos"></div>
-            </section>
-    
-            <section className="mais_prods">
-    
-            </section>
-    
-            <Footer></Footer>
-        </div>
-      );
+  function findProd(value) {
+    let prod = listaProdutos.find((produto) => produto.id === value);
+    setProdutoInfo(prod);
   }
+
+  return (
+    <div>
+      <Header />
+      
+      <Container>
+        {/* Produto Principal */}
+        <section className="prod_escolhido my-5">
+          <Row>
+            {/* Coluna da imagem */}
+            <Col md={6} className="imgs_prod d-flex justify-content-center">
+              <img src={produtoInfo.imagem} alt={produtoInfo.nome} className="img_inicial" />
+            </Col>
+            
+            {/* Coluna de informações do produto */}
+            <Col md={6} className="info_prod">
+              <h2>{produtoInfo.nome}</h2>
+              <p className="descricao">{produtoInfo.descricao}</p>
+              <p className="preco">Preço: R$ {Number(produtoInfo.preco).toFixed(2).replace(".", ",")}</p>
+              <p className="desc">Com Desconto: R$ {(produtoInfo.preco * (1 - produtoInfo.desconto)).toFixed(2).replace(".", ",")}</p>
+              {/* <p className="desc">Desconto: {produtoInfo.desconto * 100}%</p> */}
+
+              <h3>Quantidade:</h3>
+              <div className="div_btn_quant d-flex align-items-center">
+                <button className="aumentar">+</button>
+                <p className="quant mx-3 m-1">12</p>
+                <button className="diminuir">-</button>
+              </div>
+              <button className="btn_compra mt-3">COMPRAR</button>
+            </Col>
+          </Row>
+        </section>
+
+         {/* Informações de Entrega */}
+         <section className="info_entrega_prod my-4">
+          <Row>
+            <Col md={4} className="infos p-3 text-center">
+              <p className="information-prods">Devolução grátis. Você tem 30 dias a partir da data de recebimento.</p>
+              <FaUndo className="info-icon mt-2" />
+            </Col>
+            <Col md={4} className="infos p-3 text-center">
+              <p className="information-prods">Receba o produto que está esperando ou devolvemos o dinheiro.</p>
+              <FaShieldAlt className="info-icon mt-2" />
+            </Col>
+            <Col md={4} className="infos p-3 text-center">
+              <p className="information-prods">Segurança completa ao envio da <br/> vegetação.</p>
+              <FaTruck className="info-icon mt-2" />
+            </Col>
+          </Row>
+        </section>
+
+        {/* Mais produtos */}
+        <section className="mais_prods my-4">
+          <h3>Veja também</h3>
+          
+        </section>
+      </Container>
+
+      <Footer />
+    </div>
+  );
+}
 
 export default Produto;
