d67a3c107c2192b10a626c2db1b49626cde2d0dd
Luis Henrique <duimbispo@gmail.com>
2024-11-20 16:12:10 -0300

    Mudando url base da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // URL da API
 const api = axios.create({
-    baseURL: 'http://localhost:23678/api/'
+    //baseURL: 'http://localhost:22065/api/',
+    baseURL:"http://api-plantsp.runasp.net/api/"
 });
 
 export default api;
